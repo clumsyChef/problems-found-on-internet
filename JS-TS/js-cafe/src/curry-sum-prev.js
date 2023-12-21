187c8b9a861721e942ba205602f7ff62d5d802e9
Sarthak Jha <sarthakjha94@gmail.com>
2023-12-21 16:01:06 +0530

    curry-sum-prev

---
+++ JS-TS/js-cafe/src/curry-sum-prev.js
@@ -0,0 +1,17 @@
+//
+
+const prevSummation = () => {
+	let summation = 0;
+	return (numb = 0) => {
+		summation += numb;
+		return summation;
+	};
+};
+
+const sum = prevSummation();
+
+console.log(sum(1));
+console.log(sum(3));
+console.log(sum(3));
+console.log(sum(4));
+console.log(sum());
