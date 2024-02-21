26201b4e86872bdfd01a2c9f8762e5ce9c943f11
Sarthak Jha <sarthakjha94@gmail.com>
2024-02-21 18:55:57 +0530

    added-learners-bucket

---
+++ learners-bucket/vanilla-js/src/balancedParantheses.js
@@ -0,0 +1,35 @@
+// console.log("hey this is running");
+
+const areParenthesesBalanced = (str) => {
+	const myStack = [];
+	if (str.length % 2 !== 0) return false;
+
+	const myMap = {
+		"}": "{",
+		"]": "[",
+		")": "(",
+	};
+
+	for (let i = 0; i < str.length; i++) {
+		const current = str[i];
+		if (Object.keys(myMap).includes(current)) {
+			if (!myStack.length) return false;
+			const lastItem = myStack.slice(-1)[0];
+			if (lastItem !== myMap[current]) {
+				return false;
+			} else {
+				myStack.pop();
+			}
+		} else {
+			myStack.push(current);
+		}
+	}
+
+	return true;
+};
+
+console.log(areParenthesesBalanced("]["));
+
+// console.log(areParenthesesBalanced("[{}]"));
+// console.log(areParenthesesBalanced("[{}{}{}{]"));
+// console.log(areParenthesesBalanced("({[]}){}[][({})]"));
